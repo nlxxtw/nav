0b329975439857ea2db78c2fd26428b3215080e2
nlxxtw <49474003+nlxxtw@users.noreply.github.com>
2025-04-03 14:53:03 +0000

    Deploying to gh-pages from @ nlxxtw/nav@287cd02ccb2307db7dcdc8c96b88721c9cf15d33 🚀

--- chunk-BE3EZ4NA.js
+++
@@ -1,1 +0,0 @@
-import{c as a}from"./chunk-LQ2LX4XI.js";import{Ab as e,qb as r}from"./chunk-AAA2RUV6.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
